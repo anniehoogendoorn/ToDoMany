5cd5fda45bcee34ea7eaa2f8410ea7a67b1ed416
Sam Martinez & Kelli Sommerdyke <student@epicodus.com>
2015-08-18 10:04:31 -0700

    Initial commit

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
-    require_once __DIR__."/../src/Category.php"
+    require_once __DIR__."/../src/Category.php";
 
 
     $app = new Silex\Application();
@@ -20,7 +20,7 @@
         return $app['twig']->render('index.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->get("/tasks", function use ($app) {
+    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
@@ -37,17 +37,17 @@
     $app->post("/categories", function () use ($app) {
         $category = new Category($_POST['name']);
         $category->save();
-        return $app['twig']->render'categories.html.twig', array('categories' => Category::getAll()));
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
-    $app->post("/delete_category", function() use ($app) {
+    $app->post("/", function() use ($app) {
         Category::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('delete_categories.html.twig');
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('delete_tasks.html.twig');
     });
 
     return $app;
