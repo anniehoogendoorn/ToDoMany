c5c4dcc17b6b945e8672940dc9c5375ec214eb90
Kelli Sommerdyke & Annie Hoogendoorn <student@epicodus.com>
2015-08-24 11:46:57 -0700

    Remove category_id from Task class.

--- src/Task.php
+++ src/Task.php
@@ -2,29 +2,19 @@
 class Task
 {
     private $description;
-    private $category_id;
     private $id;
-    private $date;
-
 
     //Constructors
-    function __construct($description, $id = null, $category_id, $date)
+    function __construct($description, $id = null)
     {
         $this->description = $description;
         $this->id = $id;
-        $this->category_id = $category_id;
-        $this->date = $date;
     }
 
     //Setters
     function setDescription($new_description)
     {
         $this->description = (string) $new_description;
-    }
-
-    function setDate($new_date)
-    {
-        $this->date = (string) $new_date;
     }
 
     //Getters
@@ -38,33 +28,22 @@ class Task
       return $this->id;
     }
 
-    function getCategoryId()
-    {
-        return $this->category_id;
-    }
-
-    function getDate()
-    {
-        return $this->date;
-    }
-
     //Save function
     function save()
     {
-      $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id, date) VALUES ('{$this->getDescription()}', {$this->getCategoryId()}, '{$this->getDate()}');");
+      $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}', {$this->getCategoryId()});");
       $this->id = $GLOBALS['DB']->lastInsertId();
     }
 
     //Get All function
     static function getAll()
     {
-        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks ORDER BY date;");
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
         $tasks = array();
         foreach($returned_tasks as $task) {
           $description = $task['description'];
           $id = $task['id'];
-          $category_id = $task['category_id'];
-          $new_task = new Task($description, $id, $category_id);
+          $new_task = new Task($description, $id);
           array_push($tasks, $new_task);
         }
         return $tasks;
@@ -89,6 +68,17 @@ class Task
         }
       }
       return $found_task;
+    }
+
+    function update($new_description)
+    {
+      $GLOBALS['DB']->exec("UPDAT tasks SET description = '{new_description}' WHERE id = {$this->getId()};");
+      $this->setDescription($new_description);
+    }
+
+    function delete()
+    {
+      $GLOBALS['DB']->exec("DELETE FROM tasks WHERE id = {$this->getId()};");
     }
 }
 
